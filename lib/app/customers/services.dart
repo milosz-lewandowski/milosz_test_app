f55ef7b52106402489157578ebb31f51f4153deb
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-03 22:57:09 +0200

    App consumes customer Api message

---
+++ lib/app/customers/services.dart
@@ -0,0 +1,1 @@
+//funkcje do pobierania
--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
 Future fetchFromApi(String apiPath) async {
